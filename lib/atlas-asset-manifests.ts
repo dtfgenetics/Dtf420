5b6036f14f7a0636f55f92ada1f0fced46d906fc
dtfgenetics <green801028@gmail.com>
2026-08-24 18:47:13 -0500

    Aggregate growth and diagnostic Atlas asset manifest

--- lib/atlas-asset-manifests.ts
+++ lib/atlas-asset-manifests.ts
@@ -1,4 +1,9 @@
 import baseOverrides from "@/content/atlas-asset-overrides.json";
 import coreOverrides from "@/content/atlas-asset-overrides-core-structure.json";
+import growthDiagnosticOverrides from "@/content/atlas-asset-overrides-growth-diagnostic.json";
 
-export const atlasAssetOverrides = [...baseOverrides, ...coreOverrides];
+export const atlasAssetOverrides = [
+  ...baseOverrides,
+  ...coreOverrides,
+  ...growthDiagnosticOverrides,
+];
