c72c051582ec43c9e4618830a0d4b640a7217d54
dtfgenetics <green801028@gmail.com>
2026-08-24 18:43:26 -0500

    Add Atlas asset manifest aggregator

---
+++ lib/atlas-asset-manifests.ts
@@ -0,0 +1,4 @@
+import baseOverrides from "@/content/atlas-asset-overrides.json";
+import coreOverrides from "@/content/atlas-asset-overrides-core-structure.json";
+
+export const atlasAssetOverrides = [...baseOverrides, ...coreOverrides];
