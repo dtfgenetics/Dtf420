3481ec182877aa20c2126d7626c3a43185533b69
dtfgenetics <green801028@gmail.com>
2026-08-24 18:57:15 -0500

    Aggregate final Atlas asset manifest

--- lib/atlas-asset-manifests.ts
+++ lib/atlas-asset-manifests.ts
@@ -2,10 +2,12 @@ import baseOverrides from "@/content/atlas-asset-overrides.json";
 import coreOverrides from "@/content/atlas-asset-overrides-core-structure.json";
 import growthDiagnosticOverrides from "@/content/atlas-asset-overrides-growth-diagnostic.json";
 import finalAOverrides from "@/content/atlas-asset-overrides-final-a.json";
+import finalBOverrides from "@/content/atlas-asset-overrides-final-b.json";
 
 export const atlasAssetOverrides = [
   ...baseOverrides,
   ...coreOverrides,
   ...growthDiagnosticOverrides,
   ...finalAOverrides,
+  ...finalBOverrides,
 ];
