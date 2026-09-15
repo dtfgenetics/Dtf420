d537d86db04178023defdbf33145d2b976e5a7d9
dtfgenetics <green801028@gmail.com>
2026-09-15 14:38:32 -0500

    fix: use valid manifest icon purpose

--- app/manifest.ts
+++ app/manifest.ts
@@ -15,7 +15,7 @@ export default function manifest(): MetadataRoute.Manifest {
         src: "/favicon",
         sizes: "512x512",
         type: "image/png",
-        purpose: "any maskable",
+        purpose: "any",
       },
     ],
   };
